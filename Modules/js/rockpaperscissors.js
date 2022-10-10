b758ea3da34edeb5f0626db672aab9d4eac5fe38
BrendanFreeman1 <vollak1@gmail.com>
2022-10-10 14:24:54 +1100

    Finished Rock,Paper,Scissors logic
    Added game loop that replays the game until either the user or ai has won 3 times

--- Modules/js/rockpaperscissors.js
+++ Modules/js/rockpaperscissors.js
@@ -1,70 +1,72 @@
 //Declare variables
-const rock = "rock", paper = "paper", scissors = "scissors";
-const draw = "draw", user = "user", ai = "ai";
+const rock = "rock",
+  paper = "paper",
+  scissors = "scissors";
+const draw = "draw",
+  user = "user",
+  ai = "ai";
 let userScore = 0;
 let aiScore = 0;
 let drawScore = 0;
+let winner = "";
+let stillPlaying = true;
 
-//Get random selection for computer
-let rndNumber = Math.random();
-let aiSelection = CalcAIResult(rndNumber);
+while(stillPlaying)
+{
+  playRound();
 
-//Prompt user for selection and Get selection from user
-let userSelection = prompt("Please select either: Rock, Paper or Scissors");
+  console.log( `The winner was: ${winner}`)
+  console.log(`The Players Score is: ${userScore}`)
+  console.log(`The Players Score is: ${aiScore}`);
+}
 
-console.log(rndNumber);
-console.log(aiSelection);
-console.log(userSelection);
-let winner = calcGameResult(aiSelection, userSelection);
-console.log(winner);
+function playRound() 
+{
+    //Get random selection for computer
+    let rndNumber = Math.random();
+    let aiSelection = CalcAIResult(rndNumber);
 
-//Increase player or AI tally by 1 
-if(winner === draw) drawScore++;
-if(winner === user) userScore++;
-if(winner === ai) aiScore++;
+    //Prompt user for selection and Get selection from user
+    let userSelection = prompt("Please select either: Rock, Paper or Scissors").toLowerCase();
 
-//Display result
-console.log(`The Ai selected: ${aiSelection}`);
-console.log(`The User selected: ${userSelection}`);
-console.log(`The Winner is: ${winner}`);
-//alert(`The Winner was ${winner}. The score is Draw: ${drawScore} Player: ${userScore} AI: ${aiScore}`);
+    //Get Winner
+    winner = calcGameResult(aiSelection, userSelection);
+    increaseTally(winner);
+}
 
+function increaseTally(winner) 
+{
+    if (winner === draw) drawScore++;
+    if (winner === user) userScore++;
+    if (winner === ai) aiScore++;
 
-
+    if (userScore >= 3 || aiScore >= 3) stillPlaying = false;
+}
 
 //Calculate AI result
-function CalcAIResult(rndNumber)
+function CalcAIResult(rndNumber) 
 {
-    if(rndNumber <= 0.33)
-    {
-        return rock;
-    }
-    else if (rndNumber >= 0.66) 
-    {
-        return scissors;
-    }
-    else
-    {
-        return paper;
-    }
+    if (rndNumber <= 0.33) return rock;
+    else if (rndNumber >= 0.66) return scissors;
+    else return paper;
 }
 
 //Calculate game result
-function calcGameResult(aiSelection, userSelection)
+function calcGameResult(aiSelection, userSelection) 
 {
     if (aiSelection === rock) {
-      if (userSelection === rock) return draw;
-      if (userSelection === paper) return user;
-      if (userSelection === scissors) return ai;
+        if (userSelection === rock) return draw;
+        if (userSelection === paper) return user;
+        if (userSelection === scissors) return ai;
     }
     if (aiSelection === paper) {
-      if (userSelection === rock) return ai;
-      if (userSelection === paper) return draw;
-      if (userSelection === scissors) return user;
+        if (userSelection === rock) return ai;
+        if (userSelection === paper) return draw;
+        if (userSelection === scissors) return user;
     }
     if (aiSelection === scissors) {
-      if (userSelection === rock) return user;
-      if (userSelection === paper) return ai;
-      if (userSelection === scissors) return draw;
+        if (userSelection === rock) return user;
+        if (userSelection === paper) return ai;
+        if (userSelection === scissors) return draw;
     }
 }
