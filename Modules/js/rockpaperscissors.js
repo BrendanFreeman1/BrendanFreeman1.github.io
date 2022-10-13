08dcf05b7af3923246343adf315be3b70e91a036
BrendanFreeman1 <vollak1@gmail.com>
2022-10-13 16:23:02 +1100

    Added game interaction
    Added buttons for player to select rock, paper or scissors.
    Added text that updates with current game score.

--- Modules/js/rockpaperscissors.js
+++ Modules/js/rockpaperscissors.js
@@ -1,66 +1,70 @@
-const rock = "rock",
-  paper = "paper",
-  scissors = "scissors";
-const draw = "draw",
-  user = "user",
-  ai = "ai";
+const rock = "rock", paper = "paper", scissors = "scissors";
+const draw = "draw", user = "user", ai = "ai";
 let userScore = 0;
 let aiScore = 0;
 let drawScore = 0;
 let winner = "";
 let stillPlaying = true;
+let userSelection = "";
+const gameText = document.querySelector(".gameText");
+const rockBtn = document.querySelector(".rock");
+const paperBtn = document.querySelector(".paper");
+const scissorsBtn = document.querySelector(".scissors");
+rockBtn.addEventListener("click", () => {playRound(rock);});
+paperBtn.addEventListener("click", () => {playRound(paper);});
+scissorsBtn.addEventListener("click", () => {playRound(scissors);});
 
-while(stillPlaying)
+updateGameText();
+
+function playRound(userSelection) 
 {
-  playRound();
+  let rndNumber = Math.random();
+  let aiSelection = CalcAIResult(rndNumber);
 
-  console.log( `The winner was: ${winner}`)
-  console.log(`The Players Score is: ${userScore}`)
-  console.log(`The Players Score is: ${aiScore}`);
+  winner = calcGameResult(aiSelection, userSelection);
+  increaseTally(winner);
+
+  updateGameText();
 }
 
-function playRound() 
+function updateGameText() 
 {
-    let rndNumber = Math.random();
-    let aiSelection = CalcAIResult(rndNumber);
-
-    let userSelection = prompt("Please select either: Rock, Paper or Scissors").toLowerCase();
-
-    winner = calcGameResult(aiSelection, userSelection);
-    increaseTally(winner);
+  gameText.textContent = `Player Score: ${userScore} AI Score: ${aiScore} Draws: ${drawScore}`;
 }
 
 function increaseTally(winner) 
 {
-    if (winner === draw) drawScore++;
-    if (winner === user) userScore++;
-    if (winner === ai) aiScore++;
+  if (winner === draw) drawScore++;
+  if (winner === user) userScore++;
+  if (winner === ai) aiScore++;
 
-    if (userScore >= 3 || aiScore >= 3) stillPlaying = false;
+  if (userScore >= 1 || aiScore >= 1) endGame();
 }
+
+function endGame() {}
 
 function CalcAIResult(rndNumber) 
 {
-    if (rndNumber <= 0.33) return rock;
-    else if (rndNumber >= 0.66) return scissors;
-    else return paper;
+  if (rndNumber <= 0.33) return rock;
+  else if (rndNumber >= 0.66) return scissors;
+  else return paper;
 }
 
 function calcGameResult(aiSelection, userSelection) 
 {
-    if (aiSelection === rock) {
-        if (userSelection === rock) return draw;
-        if (userSelection === paper) return user;
-        if (userSelection === scissors) return ai;
-    }
-    if (aiSelection === paper) {
-        if (userSelection === rock) return ai;
-        if (userSelection === paper) return draw;
-        if (userSelection === scissors) return user;
-    }
-    if (aiSelection === scissors) {
-        if (userSelection === rock) return user;
-        if (userSelection === paper) return ai;
-        if (userSelection === scissors) return draw;
-    }
+  if (aiSelection === rock) {
+    if (userSelection === rock) return draw;
+    if (userSelection === paper) return user;
+    if (userSelection === scissors) return ai;
+  }
+  if (aiSelection === paper) {
+    if (userSelection === rock) return ai;
+    if (userSelection === paper) return draw;
+    if (userSelection === scissors) return user;
+  }
+  if (aiSelection === scissors) {
+    if (userSelection === rock) return user;
+    if (userSelection === paper) return ai;
+    if (userSelection === scissors) return draw;
+  }
 }
