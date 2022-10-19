79c4a5d9a6aa1d5d8d05b794f1498ae9d8bfec89
BrendanFreeman1 <vollak1@gmail.com>
2022-10-19 12:06:08 +1100

    Finished page layout, adjusting logic flow
    Finished positioning elements on page with CSS.
    Adjusting JS code to simplify logic flow and improve readability of code.

--- Modules/js/rockpaperscissors.js
+++ Modules/js/rockpaperscissors.js
@@ -1,90 +1,86 @@
+//Constants
 const ROCK = "Rock 🪨", PAPER = "Paper 📄", SCISSORS = "Scissors ✂️";
-const DRAW = "Draw", USER = "Player", AI = "AI";
+const DRAW = "Draw", USER = "Player", AI = "Ai";
+
+//Game Stats
 let userScore = 0;
 let aiScore = 0;
 let roundWinner = "";
 let userSelection = "";
+let aiSelection = "";
+
+//DOM Elements
+const scoreText = document.querySelector(".scoreText");
+const playerScoreText = document.querySelector(".playerScoreText");
+const aiScoreText = document.querySelector(".AIScoreText");
+const startText = document.querySelector(".startText");
 const selectionText = document.querySelector(".selectionText");
-const roundWinnerText = document.querySelector(".roundWinner");
-const startText = document.querySelector(".startText")
-const scoreText = document.querySelector(".score");
-const playerScoreText = document.querySelector(".playerScore");
-const aiScoreText = document.querySelector(".AIScore");
-const gameWinner = document.querySelector(".gameWinner");
+const roundWinnerText = document.querySelector(".roundWinnerText");
+
+//Buttons
 const rockBtn = document.querySelector(".rock");
 const paperBtn = document.querySelector(".paper");
 const scissorsBtn = document.querySelector(".scissors");
-const resetBtn = document.querySelector(".reset");
-
-resetBtn.style.display = "none";
 rockBtn.addEventListener("click", () => {playRound(ROCK);});
 paperBtn.addEventListener("click", () => {playRound(PAPER);});
 scissorsBtn.addEventListener("click", () => {playRound(SCISSORS);});
-resetBtn.addEventListener("click", () => resetGame());
 
 
 function playRound(userSelection) 
 {
+  if(userScore >= 5 || aiScore >= 5) resetGame();
+  
   updateGameText();
 
-  let rndNumber = Math.random();
-  let aiSelection = CalcAIResult(rndNumber);
-
+  aiSelection = calcAIResult();
   roundWinner = calcGameResult(aiSelection, userSelection);
+
   increaseTally(roundWinner);
   updateGameText(userSelection, aiSelection, roundWinner);
 }
 
+function increaseTally(roundWinner) {
+  if (roundWinner === USER) userScore++;
+  if (roundWinner === AI) aiScore++;
+
+  if (userScore >= 5) endGame(USER);
+  if (aiScore >= 5) endGame(AI);
+} 
+
 function updateGameText(userSelection, aiSelection, roundWinner) 
 {
-  selectionText.textContent = `${userSelection} VS ${aiSelection}`;
-  if(roundWinner === DRAW) roundWinnerText.textContent = `It's a ${roundWinner}`;
-  else roundWinnerText.textContent = `${roundWinner} wins this round`;
-  
-  startText.textContent = "";
+  startText.style.display = "none";
   scoreText.textContent = "SCORE";
   playerScoreText.textContent = `Player: ${userScore}`;
   aiScoreText.textContent = `AI: ${aiScore}`;
+
+  selectionText.textContent = `${userSelection} VS ${aiSelection}`;
+
+  if(roundWinner === DRAW) roundWinnerText.textContent = `It's a ${roundWinner}`;
+  else roundWinnerText.textContent = `${roundWinner} wins this round`;
 }
-
-function increaseTally(roundWinner) {
-  if (roundWinner === USER) userScore++;
-  if (roundWinner === AI) aiScore++;  
-
-  if(userScore >= 5) endGame(USER);
-  if(aiScore >= 5) endGame(AI);
-} 
 
 function endGame(winner) 
 {
-  gameWinner.textContent = `The Winner is ${winner} `;
-  rockBtn.disabled = true;
-  paperBtn.disabled = true;
-  scissorsBtn.disabled = true;
-  resetBtn.style.display = "block";
+  selectionText.textContent = `The Winner is ${winner} `;
+  roundWinnerText.textContent = "Make a selection to start again"
 }
 
 function resetGame() {
+  //Reset data
   userScore = 0;
   aiScore = 0;
   drawScore = 0;
   roundWinner = "";
   userSelection = "";
-  
-  rockBtn.disabled = false;
-  paperBtn.disabled = false;
-  scissorsBtn.disabled = false;
   updateGameText();
-  resetBtn.style.display = "none";
-
-  startText.textContent = "Make Your Choice!";
-  playerSelectionText.textContent = ``;
-  AISelectionText.textContent = ``;
-  roundWinnerText.textContent = ``;
 }
 
-function CalcAIResult(rndNumber) 
+function calcAIResult() 
 {
+  let rndNumber = Math.random();
+  
+  
   if (rndNumber <= 0.33) return ROCK;
   else if (rndNumber >= 0.66) return SCISSORS;
   else return PAPER;
