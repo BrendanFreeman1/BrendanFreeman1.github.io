cf127aeed9fd2e2f3ce947cfd8e5fdc6abf49abd
BrendanFreeman1 <vollak1@gmail.com>
2022-10-20 16:30:59 +1100

    Added settings menu
    Added div to contain settings items.
    Added colour picker which successfuly changes the colour the user draws in.
    Added value slider to select grid size and text to show user what size they're selecting.
    Changing grid size is not functional yet.
    Added clear button to clear the sketch pad, this is also not functional.

--- Modules/js/etch-a-sketch.js
+++ Modules/js/etch-a-sketch.js
@@ -1,50 +1,73 @@
 const gridContainer = document.querySelector(".gridContainer");
-let gridSize = 32;
-let boxSize = `${600/gridSize}px`; 
-let colour = "black";
-
-let mouseDown = false;
-document.body.onload = buildGrid(gridSize);
-document.body.onmousedown = () => (mouseDown = true);
-document.body.onmouseup = () => (mouseDown = false);
+const colourPicker = document.querySelector(".colourPicker");
+const sizeSlider = document.querySelector(".sizeSlider");
+const sizeText = document.querySelector(".sizeText");
+const clearBtn = document.querySelector(".clearBtn");
 
 gridContainer.addEventListener("mouseover", (e) => { changeBoxColour(e, colour); });
+colourPicker.addEventListener("input", (e) => { colour = colourPicker.value; });
+sizeText.textContent = `${sizeSlider.value} x ${sizeSlider.value}`;
+sizeSlider.addEventListener("input", (e) => { changeGridSize(sizeSlider.value); });
+clearBtn.addEventListener("click", (e) => { clearSketch(); });
 
-function buildGrid(gridSize) 
-{
-  for(let i=0; i<gridSize; i++)
-  {
+const DEFAULT_GRID = 48;
+const DEFAULT_COLOUR = "black";
+
+let gridSize = DEFAULT_GRID;
+let boxSize = `${600 / gridSize}px`;
+let colour = DEFAULT_COLOUR;
+let mouseDown = false;
+
+loadDocument();
+
+function loadDocument() {
+  document.body.onmousedown = () => (mouseDown = true);
+  document.body.onmouseup = () => (mouseDown = false);
+  document.body.onload = buildGrid(gridSize);
+  document.body.ondragstart = () => { return false; };
+  document.body.ondrop = () => { return false; };
+}
+
+function buildGrid(gridSize) {
+  for (let i = 0; i < gridSize; i++) {
     let row = document.createElement("div");
     row.className = "row";
     row.style.height = boxSize;
 
-    for(let j=0; j<gridSize; j++)
-    {
-          let box = document.createElement("div");
-          box.id = `${i}${j}`;
-          setBoxStyle(box)
-          row.appendChild(box);
+    for (let j = 0; j < gridSize; j++) {
+      let box = document.createElement("div");
+      box.id = `${i}${j}`;
+      setBoxStyle(box);
+      row.appendChild(box);
     }
 
     gridContainer.appendChild(row);
   }
 }
 
-function setBoxStyle(box)
-{
+function setBoxStyle(box) {
   box.className = "box";
   box.style.display = "inline-block";
   box.style.width = boxSize;
   box.style.height = boxSize;
 }
 
+//Look into why it's missing boxes sometimes
 function changeBoxColour(e, colour) {
-  if (e.type === "mouseover" && !mouseDown) return;
-  // figure out which table cell it occurs within
+  if (!mouseDown) return;
+
   let td = e.target.closest(".box");
-  if (!td) return true;
+  if (!td) return;
 
   //let cellId = td.getAttribute('id')
   td.style.backgroundColor = colour;
 }
 
+function changeGridSize(gridSize)
+{
+  clearSketch();
+  gridSize = sizeSlider.value;
+  sizeText.textContent = `${sizeSlider.value} x ${sizeSlider.value}`;
+}
+
+function clearSketch() {}
