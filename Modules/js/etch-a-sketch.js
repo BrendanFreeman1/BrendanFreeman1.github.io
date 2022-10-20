cc472e2dbe452c418c2d67b0a1713a84b3cc083b
BrendanFreeman1 <vollak1@gmail.com>
2022-10-20 13:56:20 +1100

    Implemented sketch pad
    Successfuly implemented the grid for the user to draw on.
    Drawing works while the user has a mouse button down.

--- Modules/js/etch-a-sketch.js
+++ Modules/js/etch-a-sketch.js
@@ -1,13 +1,50 @@
-let gridSize = 16;
 const gridContainer = document.querySelector(".gridContainer");
+let gridSize = 32;
+let boxSize = `${600/gridSize}px`; 
+let colour = "black";
+
+let mouseDown = false;
 document.body.onload = buildGrid(gridSize);
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
 
+gridContainer.addEventListener("mouseover", (e) => { changeBoxColour(e, colour); });
 
+function buildGrid(gridSize) 
+{
+  for(let i=0; i<gridSize; i++)
+  {
+    let row = document.createElement("div");
+    row.className = "row";
+    row.style.height = boxSize;
 
-function buildGrid(gridSize) {
-  for (let i = 0; i < gridSize; i++) {
-    let newDiv = document.createElement("div");
-    newDiv.innerText=(i)
-    gridContainer.appendChild(newDiv);
+    for(let j=0; j<gridSize; j++)
+    {
+          let box = document.createElement("div");
+          box.id = `${i}${j}`;
+          setBoxStyle(box)
+          row.appendChild(box);
+    }
+
+    gridContainer.appendChild(row);
   }
 }
+
+function setBoxStyle(box)
+{
+  box.className = "box";
+  box.style.display = "inline-block";
+  box.style.width = boxSize;
+  box.style.height = boxSize;
+}
+
+function changeBoxColour(e, colour) {
+  if (e.type === "mouseover" && !mouseDown) return;
+  // figure out which table cell it occurs within
+  let td = e.target.closest(".box");
+  if (!td) return true;
+
+  //let cellId = td.getAttribute('id')
+  td.style.backgroundColor = colour;
+}
+
