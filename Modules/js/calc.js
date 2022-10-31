caeb2d1ad64a46c65cd2ae2cb7b3668dd985c0d4
BrendanFreeman1 <vollak1@gmail.com>
2022-10-31 12:34:42 +1100

    Most calc functionality working
    Implemented most of the calculator functionality, many bugs remain for when it is used improperly

--- Modules/js/calc.js
+++ Modules/js/calc.js
@@ -1,164 +1,195 @@
 //#region Operation Class
-class operation {
-  Constructor(operator, num1, num2) {
+class Operation {
+  constructor(operator, firstNum, secondNum) {
     this.operator = operator;
-    this.num1 = num1;
-    this.num2 = num2;
+    this.firstNum = firstNum;
+    this.secondNum = secondNum;
   }
 }
 //#endregion
 
 //#region Initialise variables
 const equationText = document.querySelector(".equationText");
-const answerText = document.querySelector(".answerText");
+const currentNum = document.querySelector(".currentNum");
 const clearAllBtn = document.querySelector(".clearAllBtn");
 const backSpaceBtn = document.querySelector(".backSpaceBtn");
 const plusMinusBtn = document.querySelector(".plusMinusBtn");
 const numButtons = document.querySelectorAll(".numBtn");
 const operatorBtn = document.querySelectorAll(".operatorBtn");
 
-let equationStart = true;
+let endEquation = false;
+let currentEquation = new Operation("0","","");
 
-clearAllBtn.addEventListener("click", () => {  ClearAnswer(); ClearEquation(); });
-backSpaceBtn.addEventListener("click", () => BackSpace(answerText.textContent));
-plusMinusBtn.addEventListener("click", () => PlusMinus(answerText.textContent));
-numButtons.forEach((button) => button.addEventListener("click", () => AppendEquationText(button.textContent)));
+clearAllBtn.addEventListener("click", () => { ClearAll(currentEquation); });
+backSpaceBtn.addEventListener("click", () => BackSpace(currentNum.textContent));
+plusMinusBtn.addEventListener("click", () => PlusMinus(currentNum.textContent));
+numButtons.forEach((button) => button.addEventListener("click", () => AppendCurrentNum(button.textContent)));
 operatorBtn.forEach((button) => button.addEventListener("click", () => OperatorClicked(button.textContent)));
-
+document.body.onload = ClearCurrentNum();
 //#endregion
 
 function OperatorClicked(character)
 {
-  if (equationStart) StartEquation(character);
+  if (currentEquation.firstNum === "") StartEquation(character);
   else finishEquation(character);
 }
 
-function StartEquation(character) {
+function StartEquation(character) 
+{
+  currentEquation.operator = character;
+  currentEquation.firstNum = currentNum.textContent;
 
-  let currentEquation = new operation(character, equationText.textContent, "")
-
-  AppendEquationText(" " + character + " ");
-  
-  //I dont know whats going on
+  if (currentEquation.operator === "𝑥²" || currentEquation.operator === "²√𝑥") 
+  {
+    EvaluateEquation(currentEquation);
+  } 
+  else if(currentEquation.operator === "=")
+  {
+    currentNum.textContent = currentEquation.firstNum;
+  } 
+  else 
+  {
+    equationText.textContent = currentEquation.firstNum + " " + currentEquation.operator + " ";
+    ClearCurrentNum();
+  }
 }
 
 function finishEquation(character)
 {
+  currentEquation.secondNum = currentNum.textContent;
 
+  if(character === "=")
+  {
+    EvaluateEquation(currentEquation);
+  }
+  else
+  {
+    currentEquation.firstNum = Operate(currentEquation);
+    currentEquation.operator = character;
+    equationText.textContent = currentEquation.firstNum + " " + currentEquation.operator + " ";
+    ClearCurrentNum();
+  }
 }
 
-function AppendEquationText(character) 
+function EvaluateEquation(currentEquation)
 {
-  equationText.textContent += character;
+  let answer = Operate(currentEquation);
+
+  endEquation = true;
+  currentNum.textContent = answer;
+
+  ClearEquationText();
+  ClearCurrentEquation(currentEquation);
+}
+
+function AppendCurrentNum(character) 
+{
+  if(endEquation) 
+  {
+    ClearAll(currentEquation);
+    endEquation = false;
+  }
+
+  if(currentNum.textContent === "0") BackSpace();
+
+  currentNum.textContent += character;
 }
 
 function BackSpace() 
 {
-  equationText.textContent = equationText.textContent.slice(0, -1);
+  currentNum.textContent = currentNum.textContent.slice(0, -1);
 }
 
-function ClearAnswer() 
+function ClearAll(currentEquation) 
 {
-  if(answerText.textContent !== null) { answerText.textContent = ""; }
+  ClearEquationText();
+  ClearCurrentNum();
+  ClearCurrentEquation(currentEquation);
 }
 
-function ClearEquation()
+function ClearCurrentNum() 
+{
+  if(currentNum.textContent !== null) { currentNum.textContent = "0"; }
+}
+
+function ClearEquationText()
 {
   equationText.textContent = "";
 }
 
 function PlusMinus() 
 {
-  if (equationText.textContent.toString().charAt(0) === "-") {
-    equationText.textContent = equationText.textContent.slice(1);
+  if (currentNum.textContent.toString().charAt(0) === "-") {
+    currentNum.textContent = currentNum.textContent.slice(1);
   } else {
-    equationText.textContent = "-" + equationText.textContent;
+    currentNum.textContent = "-" + currentNum.textContent;
   }
 }
 
 function ClearCurrentEquation(currentEquation)
 {
   currentEquation.operator = "";
-  currentEquation.num1 = "";
-  currentEquation.num2 = "";
+  currentEquation.firstNum = "";
+  currentEquation.secondNum = "";
 }
 
 //#region Operation functions
 
-function Operate(currentEquation) {
-  let num1 = parseInt(currentEquation.num1);
-  let num2 = parseInt(currentEquation.num2);
+function Operate(currentEquation) 
+{
+  let num1 = parseInt(currentEquation.firstNum);
+  let num2 = parseInt(currentEquation.secondNum);
 
-  if ((currentEquation.operator = "+")) return Add(num1, num2);
-  if ((currentEquation.operator = "-")) return Subtract(num1, num2);
-  if ((currentEquation.operator = "*")) return Multiply(num1, num2);
-  if ((currentEquation.operator = "/")) return Divide(num1, num2);
-  if ((currentEquation.operator = "^")) return Power(num1, num2);
-  if ((currentEquation.operator = "%")) return Percentage(num1, num2);
-  if ((currentEquation.operator = "√")) return SquareRoot(num1);
+  if ((currentEquation.operator === "+")) return Add(num1, num2);
+  if (currentEquation.operator === "−") return Subtract(num1, num2);
+  if ((currentEquation.operator === "x")) return Multiply(num1, num2);
+  if (currentEquation.operator === "÷") return Divide(num1, num2);
+  if ((currentEquation.operator === "^")) return Power(num1, num2);
+  if ((currentEquation.operator === "%")) return Percentage(num1, num2);
+  if (currentEquation.operator === "𝑥²") return Square(num1);
+  if (currentEquation.operator === "²√𝑥") return SquareRoot(num1);
 }
 
-function Add(num1, num2) {
+function Add(num1, num2) 
+{
   return num1 + num2;
 }
 
-function Subtract(num1, num2) {
+function Subtract(num1, num2) 
+{
   return num1 - num2;
 }
 
-function Multiply(num1, num2) {
+function Multiply(num1, num2) 
+{
   return num1 * num2;
 }
 
-function Divide(num1, num2) {
+function Divide(num1, num2) 
+{
   if (num2 === 0) return null;
 
   return num1 / num2;
 }
 
-function Power(num, power) {
+function Power(num, power) 
+{
   return Math.pow(num, power);
 }
 
-function Percentage(num, percentage) {
+function Percentage(num, percentage) 
+{
   return num * (percentage / 100);
 }
 
-function SquareRoot(num) {
+function Square(num)
+{
+  return num * num;
+}
+
+function SquareRoot(num) 
+{
   return Math.sqrt(num);
 }
 
 //#endregion
-
-
-
-//function OperatorClicked(character) 
-// {
-//   if(equationTextClear)
-//   {
-//     //Populate first num and operator
-//     currentEquation.operator = character;
-//     currentEquation.num1 = equationText.textContent;
-//     equationText.textContent = currentEquation.num1 + " " + currentEquation.operator;
-
-//     equationTextClear = false;
-
-//   if(!equationTextClear)
-//     console.log("here")
-//     //Populate second num and display answer
-//     currentEquation.num2 = character;
-//     equationText.textContent = equationText.textContent + " " + currentEquation.num2;
-
-//     //Get answer
-//     let answer = Operate(currentEquation);    
-//     answerText.textContent = answer;
-
-//     if(character === "=")
-//     {
-//       ClearCurrentEquation(currentEquation);
-//     }else{
-//       currentEquation.num1 = answer;
-//       currentEquation.operator = character;
-//     }
-//   }
-// }
